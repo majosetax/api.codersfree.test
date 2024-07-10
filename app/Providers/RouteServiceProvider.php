f13f947d2bc56187c33a3edb8b908f25f889eec0
majosetax <mariajolopezsc@gmail.com>
2024-07-10 11:55:55 -0500

    e

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,8 +28,9 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+                ->prefix('v1')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api-v1.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
